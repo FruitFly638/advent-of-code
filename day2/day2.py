66bb98a66cc49846268440d0d4e2fa7f740de9f4
Moritz Kodytek <moritz.kodytek@suse.com>
2023-12-02 21:58:51 +0100

    Remove debug code

--- day2/day2.py
+++ day2/day2.py
@@ -11,10 +11,7 @@ games = []
 powers = []
 with open("input.txt") as file:
     for line in file:
-        #line_digits = ''.join(filter(lambda x : x in [';', ':', ','] or x in string.digits, line))
-        #print(line_digits)
         first_split = line.split(":")
-        print(first_split)
         id = int(re.findall(r'\d+', first_split[0])[0])
         game_pulls = []
         for pull in first_split[1].split(";"):
@@ -31,21 +28,17 @@ with open("input.txt") as file:
         game = Game(id, game_pulls)
 
         minimum = game.pulls[0]
-        #print(game.pulls)
         for pull in game.pulls:
             for idx, component in enumerate(pull):
                 minimum[idx] = max([minimum[idx], component])
-        print(f"Minimum: {minimum}")
         powers.append(minimum)
 
         possible = True
         for pull in game.pulls:
-            #12 red cubes, 13 green cubes, and 14 blue cubes
             if pull[0] > 12 or pull[1] > 13 or pull[2] > 14:
                     possible = False
         if possible:
             games.append(game)
-        #print(game)
 
     print(games)
     sum = 0
